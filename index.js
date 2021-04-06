0bbd9bb7daa7fc2921371008f7b7f529cb0bf2e5
SamHHS <samcinjee@gmail.com>
2021-04-06 22:30:45 +0200

    Final update

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ var i = 0;
 var units = 'metric';
 var locations = [
     {
-        place: 'SpaceX Launchpad', 
-        lat: 50,
-        lon: 50,
+        place: 'Cape Canaveral ', 
+        lat: 28.474009,
+        lon: -80.577171
     },
      {
-        place: 'NASA Launchpad', 
-        lat: 50,
-        lon: 60
+        place: 'Kennedy Space Center', 
+        lat: 28.608389,
+        lon: -80.604333
     },
      {
-        place: 'Ruski Launchpad', 
-        lat: 50,
-        lon: 70
+        place: 'Vandenberg Space ', 
+        lat: 34.633,
+        lon: -120.613
     },
-
+     {
+        place: 'Ronald Reagan Ballistic Missile ', 
+        lat:  8.716667,
+        lon: 167.733333
+    },
+    {
+        place: 'Spaceport America', 
+        lat:  32.990278,
+        lon: -106.969722
+    },
 ];
 
 var globalMap = getAPIdata();
@@ -28,16 +37,18 @@ function getAPIdata() {
     var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v11',
-    center: [locations[i].lat,locations[i].lon],
+    center: [locations[i].lon,locations[i].lat],
     zoom: 11
     });
     return map;
 }
 
+
+
 function getWeatherData(LocationWeather){
     i = LocationWeather;
     console.log(locations[i].place);
-    var request = 'https://api.openweathermap.org/data/2.5/weather?lat='+locations[i].lon+'&lon='+locations[i].lon+'&appid=f07d064e82b0836d1d53f2e47c812e50&units='+units;
+    var request = 'https://api.openweathermap.org/data/2.5/weather?lat='+locations[i].lat+'&lon='+locations[i].lon+'&appid=f07d064e82b0836d1d53f2e47c812e50&units='+units;
     fetch(request)  
    
     .then(function(response) {
@@ -47,9 +58,19 @@ function getWeatherData(LocationWeather){
     .then(function(response) {
         var degC = Math.floor(response.main.temp);
         var icon = response.weather[0].icon;
+        var windSpeed = response.wind.speed;
         var weatherBox = document.getElementById('weather');
         var weatherDescr = document.getElementById('description');
         var weatherIcon = document.getElementById('icon');
+        var weateherWindSpeed = document.getElementById('windspeed');
+        var launchCondition = document.getElementById('launchCondition');
+        if(windspeed > 15.2){
+        weateherWindSpeed.style.color = "red";
+        }else{
+          weateherWindSpeed.style.color = "Green";  
+        }
+
+        weateherWindSpeed.innerHTML = "Windspeed: "+windSpeed+" m/s";
         weatherIcon.src = "http://openweathermap.org/img/w/"+icon+".png";
         weatherDescr.innerHTML = response.weather[0].description;
         weatherBox.innerHTML = degC + '&#176;C <br>';
@@ -61,7 +82,7 @@ function getWeatherData(LocationWeather){
 function printAll(){
     for(var i = 0; i<locations.length;i++){
         generateMarker(i);
-        generateTable(i);
+        generateRow(i);
     }
 }
 
@@ -76,31 +97,32 @@ function goToLocation(clicked_value){
 function flyToLocation(location){
     location = location;
     globalMap.flyTo({
-    center: [locations[location].lat, locations[location].lon],
+    center: [locations[location].lon,locations[location].lat],
     zoom:11,
     essential: true
     });
 }
 
-function addLocation(){
-    newPlace = document.getElementById('place').value;
-    newLat = document.getElementById('lat').value;
-    newLon = document.getElementById('lon').value;
+// function addLocation(){
+//     newPlace = document.getElementById('place').value;
+//     newLat = document.getElementById('lat').value;
+//     newLon = document.getElementById('lon').value;
 
-    if(newPlace !== "" && newLon !== "" && newLat !== "" ){
-        x = locations.length;
-        locations[x] = {
-            place: newPlace,
-            lat: newLat,
-            lon: newLon
-        };
-        generateNewBtn(x);
-        generateMarker(x);
-        generateTable(x);
-    }
-}
+//     if(newPlace !== "" && newLon !== "" && newLat !== "" ){
+//         x = locations.length;
+//         locations[x] = {
+//             place: newPlace,
+//             lat: newLat,
+//             lon: newLon
+//         };
+//         generateNewBtn(x);
+//         generateMarker(x);
+//         generateRow(x);
+//         flyToLocation(x);
+//     }
+// }
 
-function generateTable(rowNumber){
+function generateRow(rowNumber){
     var rowNumber = rowNumber;
     var table = document.getElementById('locationTable');
     var tr = document.createElement('tr');
@@ -133,7 +155,7 @@ function generateMarker(x){
     x = x;
     var tempTemp = getWeatherData(x);
     var marker = new mapboxgl.Marker()
-    .setLngLat([locations[x].lat, locations[x].lon])
-    .setPopup(new mapboxgl.Popup().setText("location "+locations[x].place+" lat "+locations[x].lat + " lon " + locations[x].lon))
+    .setLngLat([locations[x].lon, locations[x].lat])
+    .setPopup(new mapboxgl.Popup().setText("location "+locations[x].place)) //+" lat "+locations[x].lat + " lon " + locations[x].lon
     .addTo(globalMap);
 }
